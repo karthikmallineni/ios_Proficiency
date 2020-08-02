21556b288576acc3fe38f32c6a9b7f046155271b
Karthik <mkarthik.appdev@gmail.com>
2020-08-03 00:56:39 +0530

    Initial Commit

---
+++ ios_Proficiency_ExerciseTests/ios_Proficiency_ExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_Proficiency_ExerciseTests.swift
+//  ios_Proficiency_ExerciseTests
+//
+//  Created by Karthik on 03/08/20.
+//  Copyright © 2020 Administrator. All rights reserved.
+//
+
+import XCTest
+@testable import ios_Proficiency_Exercise
+
+class ios_Proficiency_ExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
